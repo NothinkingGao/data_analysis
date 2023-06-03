7a204c85f2bfa9250abb72e5339268a6e87ab711
高明无思 <119958500@qq.com>
2023-06-03 15:09:05 +0800

    ok

--- main.py
+++ main.py
@@ -33,29 +33,32 @@ print(average_age)
 # 修改average_age的索引
 average_age.index = ["老鹰", "凯尔特人", "黄蜂", "公牛", "骑士", "小牛", "掘金", "活塞", "勇士", "火箭", "步行者", "快船", "湖人", "灰熊", "热火", "雄鹿", "森林狼", "篮网", "鹈鹕", "尼克斯", "雷霆", "魔术", "76人", "太阳", "开拓者", "国王", "马刺", "猛龙", "爵士", "奇才"]
 
-# 设置列名
-average_age.name = "平均年龄"
-# 保存到csv文件
-average_age.to_csv("datas/average_age.csv")
-
-# 显示x轴全部标签
-average_age.plot(kind="line",marker="1",title="NBA球队平均年龄")
-plt.xlabel("球队")
-
-# 设置x轴的刻度
-plt.xticks(range(len(average_age.index)), average_age.index, rotation=90)
-
-
-def on_mouse_move(event):
-    if event.inaxes:
-        x = event.xdata
-        y = event.ydata
-        print(f'x={x}, y={y}')
-
-plt.connect('motion_notify_event', on_mouse_move)
-# 保存图片,中文乱码
-plt.savefig("datas/average_age.png", dpi=300, bbox_inches="tight")
+fg,ax = plt.subplots()
+ax.plot(average_age,kind="line")
 plt.show()
+# 设置列名
+# average_age.name = "平均年龄"
+# # 保存到csv文件
+# average_age.to_csv("datas/average_age.csv")
+#
+# # 显示x轴全部标签
+# average_age.plot(kind="line",marker="1",title="NBA球队平均年龄")
+# plt.xlabel("球队")
+#
+# # 设置x轴的刻度
+# plt.xticks(range(len(average_age.index)), average_age.index, rotation=90)
+#
+#
+# def on_mouse_move(event):
+#     if event.inaxes:
+#         x = event.xdata
+#         y = event.ydata
+#         print(f'x={x}, y={y}')
+#
+# plt.connect('motion_notify_event', on_mouse_move)
+# # 保存图片,中文乱码
+# plt.savefig("datas/average_age.png", dpi=300, bbox_inches="tight")
+# plt.show()
 
 
 
